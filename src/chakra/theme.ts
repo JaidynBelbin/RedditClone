a9ebcbc8945d1c270077f09afbc5cdb0c7cafa78
JaidynBluesky <jaidyn@blueskylabs.com.au>
2023-01-29 13:15:51 +0800

    Initial commit.

---
+++ src/chakra/theme.ts
@@ -0,0 +1,24 @@
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    100: "#FF3c00",
+  },
+};
+
+const fonts = {
+  body: "Open Sans, sans-serif",
+};
+
+const styles = {
+  global: () => ({
+    body: {
+      bg: "gray.200",
+    },
+  }),
+};
+
+export const theme = extendTheme({ colors, fonts, styles });
